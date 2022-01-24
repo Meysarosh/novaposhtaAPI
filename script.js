1d29ebabb447dda3d496da0b1b572985c5bf6391
Meysarosh <meysarosh.roman@gmail.com>
2022-01-24 19:59:17 +0200

    header-main page transition

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // function capitalize(word) {
 //     return word[0].toUpperCase() + word.substring(1).toLowerCase();
 // }
-const head = document.querySelector('.header');
+const header = document.querySelector(".header");
+const main = document.querySelector(".main");
+
+header.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("btn")) return;
+  header.style.opacity = "0";
+  header.style.visibility = "hidden";
+  main.style.visibility = "visible";
+  main.style.opacity = "1";
+});
